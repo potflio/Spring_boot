e58ba53980fde8c130c60f07375e74363e1b2921
PrakashPandian <120122316+potflio@users.noreply.github.com>
2025-04-11 22:07:15 +0530

    Delete view_elements directory

--- view_elements/src/main/java/com/example/demo/repository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import com.example.demo.Stumodel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repository extends JpaRepository<Stumodel,Long>{
-    
-}
--- view_elements/src/main/java/com/example/spring/repository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.spring;
-
-import com.example.spring.model;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repository extends JpaRepository<model,Long>{
-    
-}
